2ef200ce7f16ab1151cc8f6db2e17e93a834ca58
BlincheG <blinchegg@gmail.com>
2018-05-13 15:03:14 +0600

    Need fix media

--- src/js/app.js
+++ src/js/app.js
@@ -69,6 +69,27 @@ $(document).ready(function(){
     $('.sliders').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 1,
+        mobileFirst:true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 990,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 });
